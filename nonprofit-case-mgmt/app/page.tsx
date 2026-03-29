89762e425aad724bbbc2bb285c4381917864d62d
Wendyc07 <wcejagon@asu.edu>
2026-03-29 01:07:44 -0700

    Revert "Update page.tsx"
    This reverts commit 25d07df3d9f829f834ad8f0e64e1236deca4d590.

--- nonprofit-case-mgmt/app/page.tsx
+++ nonprofit-case-mgmt/app/page.tsx
@@ -1,48 +1,65 @@
+import Image from "next/image";
 
-
-export default function Login() {
+export default function Home() {
   return (
-    <div className="flex flex-col mt-16 flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
-      
-        
-        <div className="flex flex-col items-center gap-10 text-center sm:items-start sm:text-center">
-          <h1 className="max-w-xs -mt-90 ml-70 text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Login
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            MEOW MEOW MEOW:3
           </h1>
-          <p className="max-w-md ml-46 text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Please enter your login information{" "}
-          </p>
-          <input 
-             type="text" 
-             placeholder="Email"
-             className="w-full max-w-x ml-30 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-black">
-          </input>
-          <input 
-             type="text" 
-             placeholder="Password"
-             className="w-full max-w-x mt-0 ml-30 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-black">
-          </input>
-          <p className="max-w-md ml-78 text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            OR
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
           </p>
         </div>
-        <div className="flex flex-col mt-10 ml-60 gap-4 text-base font-medium sm:flex-row">
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
-            className="flex h-14 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Google SSO
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
           </a>
         </div>
-        <p className="max-w-md ml-58 mt-13 text-lg leading-8 text-zinc-600 dark:text-zinc-400 ">
-            Forgot your password?{" "}
-          </p>
-
       </main>
     </div>
   );
 }
-
